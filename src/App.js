0a3cff54233e2bcc6ea5e67348c8ca19175be753
Rami <138556019+rami242424@users.noreply.github.com>
2025-01-03 11:37:25 +0900

    2.5 Pseudo Selectors part Two

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import styled, {keyframes} from "styled-components";
 
 const Wrapper = styled.div`
   display : flex;
+  height : 100vh;
+  width : 100vw;
+  justify-content : center;
+  align-items : center;
 `;
 
 const rotationAnimation = keyframes`
@@ -18,6 +22,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size : 36px;
+`;
+
 const Box = styled.div`
   height : 200px;
   width: 200px;
@@ -26,24 +34,21 @@ const Box = styled.div`
   justify-content : center;
   align-items: center;
   animation: ${rotationAnimation} 3s linear infinite;
-  span {
-    font-size : 36px;
-    &:hover {
-      font-size : 80px;
-    }
-    &:active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+      font-size : 98px;
   }
 `;
+
+
 
 
 function App() {
   return (
   <Wrapper>
     <Box>
-      <span>😘</span>
+      <Emoji>😘</Emoji>
     </Box>
+    <Emoji>😭</Emoji>
   </Wrapper>
   );
 }
