fc87d1e9444108f056957c9748dd4f933bbad306
Rami <138556019+rami242424@users.noreply.github.com>
2025-01-03 11:15:05 +0900

    #2.3 Attrs

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ const Father = styled.div`
   display : flex;
 `;
 
-const Btn = styled.button`
-  color : white;
+const Input = styled.input.attrs({required:true})`
   background-color : tomato;
-  border: 0;
-  border-radius: 15px;
 `;
 
 
@@ -16,9 +13,12 @@ const Btn = styled.button`
 function App() {
   return (
   <Father as="header">
-    <Btn>Log in</Btn>
-    <Btn as="a" href="/">Log out</Btn>
-    
+    <Input/>
+    <Input/>
+    <Input/>
+    <Input/>
+    <Input/>
+    <Input/>
   </Father>
   );
 }
